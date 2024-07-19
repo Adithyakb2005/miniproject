8bd64bb1d8b669384544adcde6ed451befb5eaa9
Adithyakb2005 <adithyakb1001@gmail.com>
2024-07-19 11:26:29 +0530

    new file added

---
+++ medical/addp.py
@@ -0,0 +1,6 @@
+def add_patient():
+    name=input("enter patient name: ")
+    age=int(input("enter patient age: "))
+    gender=input('enter patient gender: ')
+    address=input('enter patient address: ')
+    print('name:',name,'age:',age,'gender:',gender,'address:',address)
---
+++ medical/main.py
@@ -0,0 +1,11 @@
+def main():
+    while True:
+        print("\n 1.add patient ")
+        print("\n 2.add appointment ")
+        print("\n 3.add medical history ")
+        print("\n 4.view patients ")
+        print("\n 5.view appointment ")
+        print("\n 6.view medical historty ")
+        print("\n 7.exit")
+        ch=input("enter your choice:")
+        # if ch==1:
